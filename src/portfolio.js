cb43883e2fd68f2e32a2fea92a277852ff9ab6d4
HDehidi <HDehidi@github.com>
2023-06-07 22:08:16 +0400

    portfolio.js formated using prettier

--- src/portfolio.js
+++ src/portfolio.js
@@ -292,7 +292,7 @@ const achievementSection = {
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+    "With Love for Developing cool stuff, I love to write and teach others what I have.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
